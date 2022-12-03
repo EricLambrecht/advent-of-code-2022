9aea04c8948da0650ed059ed21a65cb6017354b0
Eric Lambrecht <ericlambrecht.hh@gmail.com>
2022-12-03 15:19:28 +0100

    style: do not use semicolons (prettier)

--- src/day1/index.ts
+++ src/day1/index.ts
@@ -1,11 +1,22 @@
-import fs from "fs/promises";
-import { getDirnameFromImportMetaUrl } from "../util/getDirnameFromImportMetaUrl.js";
+import fs from "fs/promises"
+import { getDirnameFromImportMetaUrl } from "../util/getDirnameFromImportMetaUrl.js"
+
+interface Elf {
+  name: string
+  calories: number[]
+}
 
 export const run = async () => {
-  const __dirname = getDirnameFromImportMetaUrl(import.meta.url);
+  const __dirname = getDirnameFromImportMetaUrl(import.meta.url)
   const input = await fs.readFile(__dirname + "/input.txt", {
     encoding: "utf8",
-  });
-  const caloriesPerElf = input.split("\n\n").map((i) => i.split("\n"));
-  console.log(caloriesPerElf);
-};
+  })
+  const caloriesPerElf = input.split("\n\n").map((rawCalories, i) => {
+    const calories = rawCalories.split("\n")
+    return {
+      name: `${i + 1}. Elf`,
+      calories,
+    }
+  })
+  console.log(caloriesPerElf)
+}
--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,24 @@
-import prompts from "prompts";
+import prompts from "prompts"
 
-console.log("ADVENT OF CODE 2022");
+console.log("ADVENT OF CODE 2022")
 
 const runDay = async (d: number) => {
-  const module = await import(`./day${d}/index.ts`);
-  await module.run();
-};
+  const module = await import(`./day${d}/index.ts`)
+  await module.run()
+}
 
-const [, , dayArg] = process.argv;
+const [, , dayArg] = process.argv
 
 if (dayArg) {
-  await runDay(Number.parseInt(dayArg));
+  await runDay(Number.parseInt(dayArg))
 } else {
   const { day } = await prompts({
     type: "number",
     name: "day",
     message: "Which day do you want to run?",
     validate: (v) => v > 0 && v < 26,
-  });
-  await runDay(day);
+  })
+  await runDay(day)
 }
 
-export {};
+export {}
