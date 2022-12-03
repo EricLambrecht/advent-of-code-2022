bb90873c886a6a1c0f1acf8c0a77db4ef0fa503a
Eric Lambrecht <ericlambrecht.hh@gmail.com>
2022-12-03 16:40:56 +0100

    refactor(boilerplate): Remove obsolete util files

--- src/util/getDirnameFromImportMetaUrl.ts
+++
@@ -1,7 +0,0 @@
-import path from "path"
-import { fileURLToPath } from "url"
-
-export const getDirnameFromImportMetaUrl = (importMetaUrl: string) => {
-  const __filename = fileURLToPath(importMetaUrl)
-  return path.dirname(__filename)
-}
--- src/util/readInput.ts
+++
@@ -1,9 +0,0 @@
-import { getDirnameFromImportMetaUrl } from "./getDirnameFromImportMetaUrl.js";
-import fs from "fs/promises";
-
-export const readInput = async (fileName = "input.txt") => {
-  const __dirname = getDirnameFromImportMetaUrl(import.meta.url)
-  return fs.readFile(__dirname + "/" + fileName, {
-    encoding: "utf8",
-  })
-}
