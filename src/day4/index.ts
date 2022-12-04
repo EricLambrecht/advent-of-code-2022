4e56a1208c54dfb3895ed6e43b81ae5014b31e40
Eric Lambrecht <ericlambrecht.hh@gmail.com>
2022-12-04 14:56:39 +0100

    feat(day4): Solve part 2

--- src/day4/index.ts
+++ src/day4/index.ts
@@ -7,8 +7,18 @@ export class Solution extends SolutionBase {
       const matches = pair.match(/(\d+)-(\d+),(\d+)-(\d+)/)! // we can be sure that there are matches since we know the input
       const [, aMin, aMax, bMin, bMax] = matches
       // The two-bitwise-operator (~~) converts the string to an integer
-      return (~~aMin >= ~~bMin && ~~aMax <= ~~bMax) || (~~aMin <= ~~bMin && ~~aMax >= ~~bMax)
+      return (
+        (~~aMin >= ~~bMin && ~~aMax <= ~~bMax) ||
+        (~~aMin <= ~~bMin && ~~aMax >= ~~bMax)
+      )
     })
-    console.log(obsoletePairs.length)
+    console.log("Part 1 – obsolete pairs:", obsoletePairs.length)
+
+    const overlappingPairs = pairsRaw.filter((pair) => {
+      const matches = pair.match(/(\d+)-(\d+),(\d+)-(\d+)/)! // we can be sure that there are matches since we know the input
+      const [, aMin, aMax, bMin, bMax] = matches
+      return !(~~aMax < ~~bMin || ~~aMin > ~~bMax)
+    })
+    console.log("Part 2 – overlapping pairs:", overlappingPairs.length)
   }
 }
