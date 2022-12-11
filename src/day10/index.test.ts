80e5911f532639eb4f3eb1105353169ced48671f
Eric Lambrecht <ericlambrecht.hh@gmail.com>
2022-12-11 15:45:12 +0100

    style(day10): Ran prettier

--- src/day10/index.test.ts
+++ src/day10/index.test.ts
@@ -1,57 +1,70 @@
 import { Solution } from "../day10"
 
-describe("Day 10", function() {
+describe("Day 10", function () {
   const solution = new Solution("./src/day10")
 
-  describe("Register is set correctly", function() {
-    it('increases cycle number for noop and nothing else', () => {
+  describe("Register is set correctly", function () {
+    it("increases cycle number for noop and nothing else", () => {
       const onCycle = jest.fn()
       let pd = { cycleNumber: 1, xRegister: 1 }
       pd = solution.executeInstruction("noop", pd, onCycle)
       expect(pd.cycleNumber).toBe(2)
       expect(pd.xRegister).toBe(1)
       expect(onCycle).toHaveBeenCalledTimes(1)
-      expect(onCycle).toHaveBeenCalledWith(expect.objectContaining({ cycleNumber: 2, xRegister: 1}))
+      expect(onCycle).toHaveBeenCalledWith(
+        expect.objectContaining({ cycleNumber: 2, xRegister: 1 })
+      )
     })
 
-    it('parses addx 3 correctly', () => {
+    it("parses addx 3 correctly", () => {
       const onCycle = jest.fn()
       let pd = { cycleNumber: 2, xRegister: 1 }
       pd = solution.executeInstruction("addx 3", pd, onCycle)
       expect(pd.cycleNumber).toBe(4)
       expect(pd.xRegister).toBe(4)
       expect(onCycle).toHaveBeenCalledTimes(2)
-      expect(onCycle).toHaveBeenCalledWith(expect.objectContaining({ cycleNumber: 3, xRegister: 1}))
-      expect(onCycle).toHaveBeenCalledWith(expect.objectContaining({ cycleNumber: 4, xRegister: 1}))
+      expect(onCycle).toHaveBeenCalledWith(
+        expect.objectContaining({ cycleNumber: 3, xRegister: 1 })
+      )
+      expect(onCycle).toHaveBeenCalledWith(
+        expect.objectContaining({ cycleNumber: 4, xRegister: 1 })
+      )
     })
 
-    it('parses addx -5 correctly', () => {
+    it("parses addx -5 correctly", () => {
       const onCycle = jest.fn()
       let pd = { cycleNumber: 4, xRegister: 4 }
       pd = solution.executeInstruction("addx -5", pd, onCycle)
       expect(pd.cycleNumber).toBe(6)
       expect(pd.xRegister).toBe(-1)
       expect(onCycle).toHaveBeenCalledTimes(2)
-      expect(onCycle).toHaveBeenCalledWith(expect.objectContaining({ cycleNumber: 5, xRegister: 4}))
-      expect(onCycle).toHaveBeenCalledWith(expect.objectContaining({ cycleNumber: 6, xRegister: 4}))
+      expect(onCycle).toHaveBeenCalledWith(
+        expect.objectContaining({ cycleNumber: 5, xRegister: 4 })
+      )
+      expect(onCycle).toHaveBeenCalledWith(
+        expect.objectContaining({ cycleNumber: 6, xRegister: 4 })
+      )
     })
-  });
+  })
 
-  describe("part 1", function() {
-    it('solves test riddle correctly', async () => {
+  describe("part 1", function () {
+    it("solves test riddle correctly", async () => {
       await expect(solution.part1("test_input.txt")).resolves.toBe(13140)
     })
-  });
+  })
 
-  describe("part 2", function() {
-    it('solves the test riddle correctly', async () => {
-      const expectedResult = "##..##..##..##..##..##..##..##..##..##..\n" +
+  describe("part 2", function () {
+    it("solves the test riddle correctly", async () => {
+      const expectedResult =
+        "##..##..##..##..##..##..##..##..##..##..\n" +
         "###...###...###...###...###...###...###.\n" +
         "####....####....####....####....####....\n" +
         "#####.....#####.....#####.....#####.....\n" +
         "######......######......######......####\n" +
         "#######.......#######.......#######....."
-      await expect(solution.part2("test_input.txt")).resolves.toBe(expectedResult)
+      await expect(solution.part2("test_input.txt")).resolves.toBe(
+        expectedResult
+      )
     })
-  });
-});
+  })
+})
--- src/day10/index.ts
+++ src/day10/index.ts
@@ -2,7 +2,7 @@ import { SolutionBase } from "../util/SolutionBase.js"
 
 const CYCLE_LENGTHS: Record<string, number> = {
   addx: 2,
-  noop: 1
+  noop: 1,
 }
 
 type ProgramData = {
@@ -26,11 +26,18 @@ export class Solution extends SolutionBase {
     let programData = { cycleNumber: 0, xRegister: 1 }
 
     for (let instruction of instructions) {
-      programData = this.executeInstruction(instruction, programData, ({ cycleNumber, xRegister }) => {
-        if ((cycleNumber + 20) % 40 === 0) {
-          signalStrengths += this.calculateSignalStrength(cycleNumber, xRegister)
+      programData = this.executeInstruction(
+        instruction,
+        programData,
+        ({ cycleNumber, xRegister }) => {
+          if ((cycleNumber + 20) % 40 === 0) {
+            signalStrengths += this.calculateSignalStrength(
+              cycleNumber,
+              xRegister
+            )
+          }
         }
-      })
+      )
     }
 
     return signalStrengths
@@ -39,23 +46,27 @@ export class Solution extends SolutionBase {
   async part2(fileName?: string) {
     const instructions = await this.readInputLines(fileName)
 
-
+    const SCREEN_WIDTH = 40
     let crtPosition = 0
     let screen = ""
     let programData = { cycleNumber: 0, xRegister: 1 }
 
     for (let instruction of instructions) {
-      programData = this.executeInstruction(instruction, programData, ({ xRegister }) => {
-        if (crtPosition === 0) {
-          screen += "\n"
-        }
+      programData = this.executeInstruction(
+        instruction,
+        programData,
+        ({ xRegister }) => {
+          if (crtPosition === 0) {
+            screen += "\n"
+          }
 
-        let spritePosition = this.getSpritePosition(xRegister)
-        const isLit = spritePosition.includes(crtPosition)
-        screen += isLit ? '#' : '.'
-        crtPosition++
-        crtPosition %= 40
-      })
+          let spritePosition = this.getSpritePosition(xRegister)
+          const isLit = spritePosition.includes(crtPosition)
+          screen += isLit ? "#" : "."
+          crtPosition++
+          crtPosition %= SCREEN_WIDTH
+        }
+      )
     }
 
     return screen
@@ -63,13 +74,20 @@ export class Solution extends SolutionBase {
 
   getSpritePosition(xRegister: number) {
     const SPRITE_LENGTH = 3
-    return Array(SPRITE_LENGTH).fill(null).map((_,i) => xRegister - i + 1)
+    return Array(SPRITE_LENGTH)
+      .fill(null)
+      .map((_, i) => xRegister - i + 1)
   }
 
-  executeInstruction(instruction: string, programData: ProgramData, onCycle: (programData: ProgramData) => void): ProgramData {
+  executeInstruction(
+    instruction: string,
+    programData: ProgramData,
+    onCycle: (programData: ProgramData) => void
+  ): ProgramData {
     let { cycleNumber, xRegister } = programData
-    const [name,value] = instruction.split( ' ')
+    const [name, value] = instruction.split(" ")
     const cycleLength = CYCLE_LENGTHS[name]
+
     for (let i = cycleLength; i > 0; i--) {
       cycleNumber++
       onCycle({ cycleNumber, xRegister })
@@ -77,6 +95,7 @@ export class Solution extends SolutionBase {
         xRegister += ~~value
       }
     }
+
     return { cycleNumber, xRegister }
   }
 
